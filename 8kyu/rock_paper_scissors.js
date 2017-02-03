1661a47b9cd9bf88b737290a5e4a3d20fd72ef7f
Olezhka <olezhkalviv@gmail.com>
2017-02-03 02:10:06 +0200

    Added rock paper scissors solution (clean)

--- 8kyu/rock_paper_scissors.js
+++ 8kyu/rock_paper_scissors.js
@@ -2,7 +2,10 @@
 // https://www.codewars.com/kata/rock-paper-scissors
 
 const rps = (p1, p2) => {
-	var win = [['paper', 'rock'], ['rock', 'scissors'], ['scissors', 'paper']]
-	return p1 == p2 ? 'Draw!'
-	: win.some(w => w[0] == p1 && w[1] == p2) ? 'Player 1 won!' : 'Player 2 won!';
+    var win = [
+        ['paper', 'rock'],
+        ['rock', 'scissors'],
+        ['scissors', 'paper']
+    ]
+    return p1 == p2 ? 'Draw!' : win.some(w => w[0] == p1 && w[1] == p2) ? 'Player 1 won!' : 'Player 2 won!';
 };
