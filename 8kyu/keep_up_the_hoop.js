e3a8c8d638a139d7f557d00b517f1d5c06f6b983
olezhkalviv <olezhkalviv@gmail.com>
2017-02-09 19:05:04 +0200

    Added one more kata

---
+++ 8kyu/keep_up_the_hoop.js
@@ -0,0 +1,6 @@
+// Keep up the hoop
+// https://www.codewars.com/kata/keep-up-the-hoop
+
+function hoopCount(n) {
+    return n < 10 ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+}
